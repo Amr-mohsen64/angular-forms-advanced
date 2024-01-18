8db031e19a3f50ef9c1b9762723be512e6fc94ff
Amr Mohsen <amrmohsendev@gmail.com>
2024-01-19 01:27:30 +0200

    added reactive login form

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,21 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from "@angular/forms";
+import { createPasswordStrengthValidator } from "../validators/password-strength.validator";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login-reactive.component.html',
-  styleUrls: ['./login-reactive.component.css']
+  selector: "login",
+  templateUrl: "./login-reactive.component.html",
+  styleUrls: ["./login-reactive.component.css"],
 })
 export class LoginReactiveComponent implements OnInit {
+  // email = new FormControl("", {
+  //   validators: [Validators.required, Validators.email],
+  //   updateOn: "blur",
+  // });
 
+  // password = new FormControl("", {
+  //   validators: [
+  //     Validators.required,
+  //     Validators.minLength(8),
+  //     createPasswordStrengthValidator(),
+  //   ],
+  // });
 
-  constructor() {
+  // loginForm = new FormGroup({
+  //   email: this.email,
+  //   password: this.password,
+  // });
 
+  constructor(private fb: FormBuilder) {}
+  //the nonNullable makes the fields string and not null at reset state
+  // constructor(private fb: NonNullableFormBuilder) {}
 
+  loginForm = this.fb.group({
+    email: this.fb.nonNullable.control("", {
+      validators: [Validators.required, Validators.email],
+      updateOn: "blur",
+    }),
+    password: [
+      "",
+      [
+        Validators.required,
+        Validators.minLength(8),
+        createPasswordStrengthValidator(),
+      ],
+    ],
+  });
+  ngOnInit() {}
+
+  login() {
+    const formValue = this.loginForm.value;
+    formValue;
   }
 
-  ngOnInit() {
-
+  reset() {
+    this.loginForm.reset();
+    this.loginForm.setErrors(null);
+    // this.loginForm.controls.email.setValue("amr");
+    console.log(this.loginForm.value);
   }
-
 }
