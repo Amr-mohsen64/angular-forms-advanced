8c5fffafe21062b19fde25791fdb539b4f810ee6
Amr Mohsen <amrmohsendev@gmail.com>
2024-01-23 23:06:12 +0200

    added form array

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,13 +1,41 @@
-import {Component} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component } from "@angular/core";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'create-course-step-3',
-  templateUrl: 'create-course-step-3.component.html',
-  styleUrls: ['create-course-step-3.component.scss']
+  selector: "create-course-step-3",
+  templateUrl: "create-course-step-3.component.html",
+  styleUrls: ["create-course-step-3.component.scss"],
 })
 export class CreateCourseStep3Component {
+  constructor(private fb: FormBuilder) {}
 
+  form = this.fb.group({
+    lessons: this.fb.array([this.createLessonForm()]),
+  });
 
+  get lessons() {
+    return this.form.controls["lessons"] as FormArray;
+  }
+
+  onDeleteLesson(index: number) {
+    if (this.lessons.length > 1) {
+      this.lessons.removeAt(index);
+    }
+  }
+  onAddLesson() {
+    this.lessons.push(this.createLessonForm());
+  }
+
+  private createLessonForm() {
+    return this.fb.group({
+      title: ["", Validators.required],
+      level: ["beginner", Validators.required],
+    });
+  }
 }
